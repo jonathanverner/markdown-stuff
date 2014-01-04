jonathanverner/markdown-stuff

commit c6e0c4c8fdebdf50498c315804236c5c465b3def

Feature(MD): Wrap sections (i.e. started by <h.> tags) into <section> tags.

md.py
@@ -9,6 +9,7 @@ import lxml
 from lxml.cssselect import CSSSelector
 from  mdx_tolatex import laTeXRenderer
 from mdx_defs import build_headings
+from postprocess import build_sections
 import logging
 import mdx_macros
 
@@ -158,19 +159,19 @@ def main():
       logger.critical('Could not open template file'+str(e))
       exit(-1)
 
-  md, html = render_md(unicode(args.document.read(),encoding='utf-8',errors='ignore'),tree=None)
+  md, lxml_tree = render_md(unicode(args.document.read(),encoding='utf-8',errors='ignore'),tree='lxml')
+  lxml_tree = build_sections(lxml_tree)
 
   if args.query:
-      html_tree = parse_html(html,tree='lxml')
       attrs = args.attrs.split(',')
-      for e in query(args.query, html_tree):
+      for e in query(args.query, lxml_tree):
           print ','.join([ attr+'='+e[attr] for attr in attrs if attr in e ])
       return
 
   if args.filter:
-      html_tree = parse_html(html,tree='lxml')
-      html = '\n'.join([lxml.etree.tostring(e) for e in filter(args.filter, html_tree)])
-
+      html = '\n'.join([lxml.etree.tostring(e) for e in filter(args.filter, lxml_tree, include_references=not args.norefs)])
+  else:
+      html = lxml.etree.tostring(lxml_tree)
 
   html_tree = parse_html(html,tree='md')
 

TODO
@@ -13,7 +13,7 @@ ELEMENTS/EXTENSIONS
 
   -- sections
      - encapsulate sections into a section element
-       (i.e. each # ... # type title starts a new section/subsection)
+       (i.e. each # ... # type title starts a new section/subsection) DONE
 
   -- proofs
      - end with a square DONE

postprocess.py
@@ -1,0 +1,51 @@
+import lxml.etree as etree
+
+import logging
+logger =  logging.getLogger(__name__)
+
+from utils import find_children_by_class, section_level
+
+
+def new_section(parent, h_node):
+    """ Creates a new section as a child of parent
+        from the heading node @h_node """
+    sec = etree.SubElement(parent,'section')
+    sec_numbers = find_children_by_class(h_node,'section_number')
+    sec.set('level',str(section_level(h_node)))
+    if len(sec_numbers) > 0:
+        sec.set('number',sec_numbers[0].text)
+        sec.set('title',sec_numbers[-1].tail)
+    return sec
+
+
+def build_sections(html_tree):
+    """ Wraps each heading together with what fallows
+        into an appropriate <section> node """
+    top_section = etree.Element('section')
+    section_stack = []
+    current_section = top_section
+    current_level = 0
+    for node in html_tree.find('body'):
+        level = section_level(node)
+        if level is None:
+            # node is not a heading, so we append it to the current section """
+            current_section.append(node)
+        else:
+            if level > current_level:
+                # Add the current section to the stack and create
+                # a new subsection which will be the new current_section
+                section_stack.append((current_section,current_level))
+                current_section = new_section(current_section,node)
+                current_level = level
+            elif level <= current_level:
+                # Back up the stack until we reach the current level
+                while level < current_level:
+                    current_section, current_level = section_stack.pop()
+                # Create a new current section
+                parent_section = section_stack[-1][0]
+                current_section = new_section(parent_section,node)
+            current_section.append(node)
+
+
+
+    return top_section

utils.py
@@ -1,0 +1,58 @@
+import mimetypes
+import re
+
+def find_children_by_class(parent,cls):
+    """ Returns a list of descendants of @parent having
+        css class @cls in their clas list """
+    ret = []
+    for ch in parent:
+        classes = ch.get('class','').split(' ')
+        if cls in classes:
+            ret.append(ch)
+        ret += find_children_by_class(ch,cls)
+    return ret
+
+def get_by_id( elements, id ):
+    """ Returns the element with id @id which is either in elements or
+        a descendant of an element in elements. If there is no such element
+        returns None """
+    for e in elements:
+        if e.get('id',None) == id:
+            return e
+        ret = get_by_id(e,id)
+        if ret is not None:
+            return ret
+    return None
+
+def section_level(node):
+    """ Returns None if node is not a heading tag
+        or the heading level """
+    if len(node.tag) != 2 or not node.tag.startswith('h'):
+      return None
+    try:
+      return int(node.tag[1])
+    except:
+      return None
+
+
+URI_LAST_PART_RE = re.compile('.*?/*([^/]*)$')
+EXTENSION_RE = re.compile('.*\.([^.]*)$')
+
+def get_uri_info( uri ):
+    """ Extracts some info from uri (filename, extension, mimetype)
+        and returns it as a dict (with None values where no info is available) """
+    ret = {}
+    m = URI_LAST_PART_RE.match(uri)
+    if m:
+        ret['filename']=m.groups()[0]
+        m = EXTENSION_RE.match(ret['filename'])
+        if m:
+            ret['extension']=m.groups()[0]
+        else:
+            ret['extension']=None
+    else:
+        ret['filename']=None
+        ret['extension']=None
+    mime_type, _ = mimetypes.guess_type(uri)
+    ret['mimetype'] = mime_type or 'application/octet-stream'
+    return ret

commit e4d83d02495747c8fb7671e58e7db038a42cd4b0

BugFix (MD): Fix a bug introduced in c6e0c4c8fdebd which distorted the html output.
The lxml.etree.tostring method converts elements with no content into the
<tag /> form, which is valid xml but invalid html and causes the resulting
html to be parsed incorrectly. One needs to supply the method='html'
optional parameter.

md.py
@@ -171,7 +171,7 @@ def main():
   if args.filter:
       html = '\n'.join([lxml.etree.tostring(e) for e in filter(args.filter, lxml_tree, include_references=not args.norefs)])
   else:
-      html = lxml.etree.tostring(lxml_tree)
+      html = lxml.etree.tostring(lxml_tree,method='html')
 
   html_tree = parse_html(html,tree='md')
 